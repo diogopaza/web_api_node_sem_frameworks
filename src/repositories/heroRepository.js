48a758666fea1cbb16dcee9f5e4459610b605741
diogo paza <diogopazacvel@gmail.com>
2020-11-24 07:26:30 -0300

    create service

---
+++ src/repositories/heroRepository.js
@@ -0,0 +1,43 @@
+const {readFile, writeFile } = require('fs/promises')
+
+class HeroRepository{
+    
+    constructor({ file }){
+        this.file = file
+    }
+
+     async _currentFileContent(){
+        return JSON.parse(await readFile(this.file))
+    }
+    async find(itemId){    
+         const all =  await this._currentFileContent()
+         if(!itemId) return all
+        
+         return all.find(({id}) => itemId === id)
+    }
+
+    async create(data){
+        const currentFile = await this._currentFileContent()
+        currentFile.push(data)
+
+        await writeFile(this.file, JSON.stringify(currentFile))
+        return data.id
+    }
+
+}
+
+module.exports = HeroRepository
+
+const heroRepository = new HeroRepository({
+    file: '../../database/data.json'
+})
+
+//heroRepository.find(2).then(console.log).catch(error => console.log('error', error))
+/*
+heroRepository.create({
+    "id":8,
+    "name":"Joao222",
+    "age":9,
+    "power":"ensino fundamental"
+
+}).then(console.log)*/
